0629e4f245fbe49b32cffd5f4eb2876bc4a230c4
Dominik <dominik.krystul@hva.nl>
2026-08-06 19:58:13 +0200

    Refactor portfolio structure and remove unused assets

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <HelloWorld />
+  <main class="app-shell">
+    <h1>Portfolio</h1>
+    <p>Welcome to my portfolio!</p>
+  </main>
 </template>
