b7d73f887371922788a345d3a7e61d97a3fafa31
Dominik <dominik.krystul@hva.nl>
2026-08-21 20:12:15 +0200

    feat(dependencies): remove unused dependencies from package.json and package-lock.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { templateCompilerOptions } from '@tresjs/core'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(templateCompilerOptions)],
+  plugins: [vue()],
 })
