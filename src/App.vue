f588faafb0a37eb737ae490d043e03c227d99650
Dominik <dominik.krystul@hva.nl>
2026-08-27 14:00:01 +0200

    feat: add @vercel/analytics for enhanced site analytics and tracking

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,11 @@
   <AppLayout>
     <RouterView />
   </AppLayout>
+  <Analytics />
 </template>
 
 <script setup lang="ts">
+import { Analytics } from '@vercel/analytics/vue'
 import { RouterView } from 'vue-router'
 import AppLayout from './layouts/AppLayout.vue'
 </script>
